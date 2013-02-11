e88072a97fc656f3fc287f1d8a7052a6003e7346
Matthew Bellis <matthew.bellis@gmail.com>
2013-02-11 00:20:55 -0500

    Fixed max likelihood.

--- learning_fitting/fitting_maximum_likelihood_method.py
+++ learning_fitting/fitting_maximum_likelihood_method.py
@@ -43,7 +43,8 @@ def negative_log_likelihood(p, x, y):
     # for each data point.
     # So replace ``1" with some function. You may want to refer to the chi-square
     # example.
-    ret = sum(pdf(p, x))
+    #ret = sum(pdf(p, x))
+    ret = sum(-np.log(pdf(p, x)))
     #ret = 1
     return ret
 
