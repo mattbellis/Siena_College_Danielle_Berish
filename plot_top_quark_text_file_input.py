02d0c292cef133bf0574bff934d00a364fc92e6d
Matt Bellis <matthew.bellis@gmail.com>
2013-02-25 17:12:32 -0500

    Modified the reading in program.

--- plot_top_quark_text_file_input.py
+++ plot_top_quark_text_file_input.py
@@ -4,12 +4,16 @@ import math
 
 import sys
 
+################################################################################
+# Figure out the number of combinations
+################################################################################
 def combinations(njets,ngroup):
     numer = math.factorial(njets)
     denom = math.factorial(ngroup)*math.factorial(njets-ngroup)
 
     return numer/denom
 
+################################################################################
 
 print sys.argv
 
@@ -21,15 +25,46 @@ else:
     print "Need to pass in an input file for the first argument!!!!"
     exit(-1)
 
-mass = np.zeros(500000)
+mass = -999*np.ones(500000)
 
-i = 0
-for line in infile:
-    vals = line.split()
-    #print vals
-    if len(vals)>=4:
-        mass[i] = float(vals[0])
-        i += 1
+content = np.array(infile.read().split()).astype('float')
+
+print content
+
+#exit()
+
+not_at_end = True
+nevent = 0
+ncolumns = 4
+
+count = 0
+nentries = len(content)
+
+while count<nentries:
+
+    print content[count]
+    njets = int(content[count])
+    ncombos = combinations(njets,3)
+    print ncombos
+
+    nvalues_for_this_event = ncolumns*ncombos
+    start = count+1
+    end = count+1+nvalues_for_this_event
+
+    values_for_this_event = content[start:end]
+
+    index = np.arange(0,nvalues_for_this_event,ncolumns)
+    m0  = values_for_this_event[index]
+    pt0 = values_for_this_event[index+1]
+    m1  = values_for_this_event[index+2]
+    pt1 = values_for_this_event[index+3]
+
+    print "event"
+    print m0,pt0,m1,pt1
+
+    count += (1+nvalues_for_this_event)
+
+
 
 plt.hist(mass[mass>0],bins=500)
 plt.show()
