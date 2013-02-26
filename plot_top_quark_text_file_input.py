b39e47702f2e40bb7092f85324975913b5305cdb
Matt Bellis <matthew.bellis@gmail.com>
2013-02-26 11:20:04 -0500

    Fixed the combinatorics.

--- plot_top_quark_text_file_input.py
+++ plot_top_quark_text_file_input.py
@@ -4,6 +4,8 @@ import math
 
 import sys
 
+import lichen.lichen as lch
+
 ################################################################################
 # Figure out the number of combinations
 ################################################################################
@@ -11,7 +13,11 @@ def combinations(njets,ngroup):
     numer = math.factorial(njets)
     denom = math.factorial(ngroup)*math.factorial(njets-ngroup)
 
-    return numer/denom
+    nremaining_jets = njets-ngroup
+    remainingjets_numer = math.factorial(nremaining_jets)
+    remainingjets_denom = math.factorial(ngroup)*math.factorial(nremaining_jets-ngroup)
+
+    return (numer/denom)*(remainingjets_numer/remainingjets_denom)
 
 ################################################################################
 
@@ -40,9 +46,10 @@ ncolumns = 4
 count = 0
 nentries = len(content)
 
+i = 0
 while count<nentries:
 
-    print content[count]
+    #print content[count]
     njets = int(content[count])
     ncombos = combinations(njets,3)
     print ncombos
@@ -59,12 +66,16 @@ while count<nentries:
     m1  = values_for_this_event[index+2]
     pt1 = values_for_this_event[index+3]
 
-    print "event"
-    print m0,pt0,m1,pt1
+    #print "event"
+    #print m0,pt0,m1,pt1
 
+    index = np.argsort(pt0)[-1]
+    mass[i] = m0[index]
+
+    i += 1
     count += (1+nvalues_for_this_event)
 
 
-
-plt.hist(mass[mass>0],bins=500)
+print "Events: ",len(mass[mass>0])
+lch.hist_err(mass[mass>0],bins=125,range=(0,1000))
 plt.show()
