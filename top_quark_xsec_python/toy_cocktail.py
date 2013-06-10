a0694bb588985439d99a5e94bfb9d53e70e81c24
Danielle Berish <berish@serenity>
2013-06-10 16:25:55 -0400

    Made a copy of the shell script and modified it

--- top_quark_xsec_python/toy_cocktail.py
+++ top_quark_xsec_python/toy_cocktail.py
@@ -6,7 +6,7 @@ import sys
 print sys.argv
 
 ################################################################################
-def scale(n,t):
+def scale(n,t):    
     xpts = np.array([])
     ypts = np.array([])
     new_ypts = np.array([])
@@ -25,7 +25,7 @@ def scale(n,t):
         new_ypts = np.append(new_ypts,new_y)
         total += new_y
         
-    print total
+    #print total
     return new_ypts,xpts
     
 
@@ -33,20 +33,15 @@ def scale(n,t):
 # Read in the file from the command line
 #================================================================
 
-#================================================================
+#===============================================================================================
 # ttbar
 infile_0 = None
 if len(sys.argv)>1:
     infile_0 = open(sys.argv[1],'r')
 else:
     print "Need to pass in an input file for the first argument!!!!"
-    exit(-1)   
-
-
-ttpts,xpts = scale(infile_0,20000)
-tot_pts = ttpts.copy()
-
-#================================================================
+    exit(-1)
+    
 # Wjets
 infile_1 = None
 if len(sys.argv)>1:
@@ -54,11 +49,7 @@ if len(sys.argv)>1:
 else:
     print "Need to pass in an input file for the first argument!!!!"
     exit(-1)
-    
-wjetspts,xpts = scale(infile_1,2500)
-tot_pts += wjetspts
 
-#================================================================
 # QCD
 infile_2 = None
 if len(sys.argv)>1:
@@ -66,12 +57,7 @@ if len(sys.argv)>1:
 else:
     print "Need to pass in an input file for the first argument!!!!"
     exit(-1)
-    
 
-qcdpts,xpts = scale(infile_2,100)
-tot_pts += qcdpts
-
-#================================================================
 # t
 infile_3 = None
 if len(sys.argv)>1:
@@ -79,12 +65,7 @@ if len(sys.argv)>1:
 else:
     print "Need to pass in an input file for the first argument!!!!"
     exit(-1)
-    
 
-tpts,xpts = scale(infile_3,200)
-tot_pts += tpts 
-
-#================================================================
 # tbar
 infile_4 = None
 if len(sys.argv)>1:
@@ -92,13 +73,27 @@ if len(sys.argv)>1:
 else:
     print "Need to pass in an input file for the first argument!!!!"
     exit(-1)
-    
+
+#===================================================================================================
+
+
+ttpts,xpts = scale(infile_0,19000)
+tot_pts = ttpts.copy()
+
+wjetspts,xpts = scale(infile_1,3000)
+tot_pts += wjetspts
+
+qcdpts,xpts = scale(infile_2,10)
+tot_pts += qcdpts
+
+tpts,xpts = scale(infile_3,600)
+tot_pts += tpts 
 
 tbarpts,xpts = scale(infile_4,300)
 tot_pts += tbarpts
 
-
-f = open("toy_cocktail.dat", 'w+')
+outfilename = "toy_cocktail_1.dat" 
+f = open(outfilename, 'w')
 output = ""
 
 #xpts = np.array([])
