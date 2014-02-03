4eae897f0b43aaf09a55c3c1fbfd8e29e41cc23c
Matt Bellis <matthew.bellis@gmail.com>
2014-02-03 14:24:16 -0500

    Added truth code.

---
+++ edmshyft_code/edmshyft_truth_exploration.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# ==============================================================================
+# ==============================================================================
+
+from ROOT import gRandom, TH1, TH1D, cout, TCanvas
+from ROOT import kRed,kBlack,kBlue
+from ROOT import TLorentzVector,TH2D
+
+import ROOT
+
+import sys
+
+import numpy as np
+
+tag = "ttbar_MC_truth"
+#tag = "data"
+
+# ==============================================================================
+#  Read in the data.
+# ==============================================================================
+
+#ROOT.gStyle.SetOptStat(111111111)
+ROOT.gStyle.SetOptStat(1)
+
+chain = ROOT.TChain("Events")
+
+for file in sys.argv[1:]:
+        chain.AddFile(file)
+
+###############################################################################
+# Assignment! 
+# Fill these hisograms
+###############################################################################
+htop_ptmax = TH1D("htop_ptmax","Highest p_{T} top jet", 80,0,800.0)
+hcsvjet_ptmax = TH1D("hcsvjet_ptmax","Highest p_{T} b-jet", 110,0.0,800.0)
+hmuon_ptmax = TH1D("hmuon_ptmax","Highest p_{T} muon", 80,0.0,800.0)
+
+hcsvjet_aftercuts = TH2D("hcsvjet_aftercuts","CSV variable vs. top p_{T}", 7,100,800,10,0,1.0)
+
+# Top Truth
+truth_str = []
+truth_str.append("floats_pfShyftTupleGenParticles_pdgId_ANA.obj")
+truth_str.append("floats_pfShyftTupleGenParticles_status_ANA.obj")
+truth_str.append("floats_pfShyftTupleGenParticles_pt_ANA.obj")
+
+p4 = TLorentzVector()
+
+#chain.SetBranchStatus('*', 1 )
+chain.SetBranchStatus('*', 0 )
+for s in truth_str:
+    chain.SetBranchStatus(s, 1 )
+
+
+nev = chain.GetEntries()
+print nev
+
+################################################################################
+# Loop over the events
+################################################################################
+for n in xrange(nev):
+    if n%10000==0:
+        print "%d of %d" % (n,nev)
+
+    chain.GetEntry(n)
+
+    print '---------------------'
+    for i in xrange(32):
+        pdg = chain.GetLeaf(truth_str[0]).GetValue(i)
+        #status = chain.GetLeaf(truth_str[1]).GetValue(i)
+        pt = chain.GetLeaf(truth_str[2]).GetValue(i)
+        print pdg,pt
+
+
+################################################################################
+if __name__=="__main__":
+    rep = ''
+    while not rep in [ 'q', 'Q' ]:
+        rep = raw_input( 'enter "q" to quit: ' )
+        if 1 < len(rep):
+            rep = rep[0]
+
